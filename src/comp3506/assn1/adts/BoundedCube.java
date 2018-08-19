e4f186ff65831969f5301734bafbecae4d5080c3
Jingwei WANG <wjw@wjw.co>
2018-08-19 23:34:22 +1000

    iterator

--- src/comp3506/assn1/adts/BoundedCube.java
+++ src/comp3506/assn1/adts/BoundedCube.java
@@ -12,6 +12,11 @@ package comp3506.assn1.adts;
  * @param <T> The type of element held in the data structure.
  */
 public class BoundedCube<T> implements Cube<T> {
+	private int length;
+	private int breadth;
+	private int height;
+	private Position[] positions;
+	private int cellNum;
 
 	/**
 	 * 
@@ -21,7 +26,94 @@ public class BoundedCube<T> implements Cube<T> {
 	 * @throws IllegalArgumentException If provided dimension sizes are not positive.
 	 */
 	public BoundedCube(int length, int breadth, int height) throws IllegalArgumentException {
+		this.length = length;
+		this.breadth = breadth;
+		this.height = height;
+		this.cellNum = 0;
+		this.positions = new Position[20000];
+	}
+
+	private class Position {
+		int x;
+		int y;
+		int z;
+		TraversableQueue<T> elements;
+	}
+	@Override
+	public void add(int x, int y, int z, T element) throws IndexOutOfBoundsException {
+		// TODO Auto-generated method stub
+		positions[cellNum] = new Position();
+		positions[cellNum].x = x;
+		positions[cellNum].y = y;
+		positions[cellNum].z = z;
+		positions[cellNum].elements.enqueue(element);
+		this.cellNum++;
+	}
+
+	@Override
+	public T get(int x, int y, int z) throws IndexOutOfBoundsException {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < this.cellNum; i++) {
+			if (positions[i].x == x && positions[i].y == y && positions[i].z == z) {
+				return positions[i].elements.iterator().next();
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public IterableQueue<T> getAll(int x, int y, int z) throws IndexOutOfBoundsException {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < this.cellNum; i++) {
+			if (positions[i].x == x && positions[i].y == y && positions[i].z == z) {
+				return positions[i].elements;
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public boolean isMultipleElementsAt(int x, int y, int z) throws IndexOutOfBoundsException {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < this.cellNum; i++) {
+			if (positions[i].x == x && positions[i].y == y && positions[i].z == z) {
+				return positions[i].elements.iterator().hasNext();
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public boolean remove(int x, int y, int z, T element) throws IndexOutOfBoundsException {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < this.cellNum; i++) {
+			if (positions[i].x == x && positions[i].y == y && positions[i].z == z) {
+				if (element.equals(positions[i].elements.iterator().next())) {
+					positions[i].elements.iterator().remove();
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public void removeAll(int x, int y, int z) throws IndexOutOfBoundsException {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < this.cellNum; i++) {
+			if (positions[i].x == x && positions[i].y == y && positions[i].z == z) {
+				for (int j = 0; j < positions[i].elements.size(); j++) {
+					positions[i].elements.dequeue();
+				}
+			}
+		}
 		
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		this.positions = null;
 	}
 	
 }
--- src/comp3506/assn1/adts/TraversableQueue.java
+++ src/comp3506/assn1/adts/TraversableQueue.java
@@ -1,6 +1,93 @@
 package comp3506.assn1.adts;
 
+import java.util.Iterator;
 
 public class TraversableQueue<T> implements IterableQueue<T> {
+	private LinkedNode<T> head;
+	private LinkedNode<T> tail;
+	private int size = 0;
+	
+	private class LinkedNode<T> {
+		LinkedNode<T> next;
+		LinkedNode<T> previous;
+		T element;
+	}
+	
+	private class Itr<T> implements Iterator<T> {
+		private LinkedNode<T> current;
+		private int index = 0;
+		
+		private Itr() {
+			LinkedNode<T> first = new LinkedNode<T>();
+			this.current = first;
+		}
+		
+		@Override
+		public boolean hasNext() {
+			if (this.current.next != null) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		@Override
+		public T next() {
+			if (index != 0) {
+				this.current = current.next;
+			}
+			index++;
+			return this.current.element;
+		}
+		
+		@Override
+		public void remove() {
+			this.current.previous.next = this.current.next;
+			this.current.next.previous = this.current.previous;
+			this.current = this.current.next;
+		}
+		
+	}
+	@Override
+	public void enqueue(T element) throws IllegalStateException {
+		LinkedNode<T> newNode = new LinkedNode<T>();
+		newNode.element = element;
+		if (size == 0) {
+			newNode.previous = null;
+			newNode.next = null;
+			this.head = newNode;
+			this.tail = newNode;
+		} else {
+			newNode.previous = this.tail;
+			newNode.next = null;
+			this.tail.next = newNode;
+			this.tail = newNode;
+		}
+		this.size++;
+	}
+	
+
+	@Override
+	public T dequeue() throws IndexOutOfBoundsException {
+		LinkedNode<T> tempNode = this.head;
+		this.head = tempNode.next;
+		this.head.previous = null;
+		this.size--;
+		return tempNode.element;
+	}
+
+	@Override
+	public int size() {
+		return this.size;
+	}
+
+	@Override
+	public Iterator<T> iterator() {
+		Itr<T> itr = new Itr<T>();
+		return itr;
+	}
+
+
+
 
 }
